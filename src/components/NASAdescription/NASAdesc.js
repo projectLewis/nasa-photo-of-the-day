55e85a0c5aa7bf202d8c546575b52a0110643979
Seth Lewis <lewiscatenary@gmail.com>
2019-07-17 21:19:47 -0500

    moved title to its own component

--- src/components/NASAdescription/NASAdesc.js
+++ src/components/NASAdescription/NASAdesc.js
@@ -1,14 +1,11 @@
 import React from "react";
 // styles
 import style from "./NASAdesc.module.css";
-import { Header, Segment, Rating } from "semantic-ui-react";
+import { Segment, Rating } from "semantic-ui-react";
 
-const NASAdesc = ({ title, description, mediaType }) => {
+const NASAdesc = ({ description, mediaType }) => {
   return (
     <div className={style.descriptionContainer}>
-      <Header as="h1" inverted>
-        {title}
-      </Header>
       {description ? (
         <>
           <Rating icon="star" defaultRating={3} maxRating={5} />
